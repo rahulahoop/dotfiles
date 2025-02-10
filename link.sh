a3f5902a7d5bbb09653a99653e9440a7aa2b9dbc
Rahul Bhonsale <rahul.bhonsale@soundcloud.com>
2025-02-10 14:08:16 +0100

    work configs

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
-ln -s $HOME/git/dotfiles/ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty/config 
-ln -s $HOME/git/dotfiles/nvim/ $HOME/.config/nvim/ 
-ln -s $HOME/git/dotfiles/fish/ $HOME/.config/fish/ 
+ln -s $HOME/git/dotfiles/ghostty/config $HOME/Library/Application\ Support/com.mitchellh.ghostty 
+ln -s $HOME/git/dotfiles/nvim/ $HOME/.config
+ln -s $HOME/git/dotfiles/fish/ $HOME/.config
 
